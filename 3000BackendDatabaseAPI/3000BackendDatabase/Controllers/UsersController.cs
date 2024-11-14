a317321753cee7560f4d05317f06696e7bd3009d
Patryk102 <patryk.rog@students.plymouth.ac.uk>
2024-11-14 16:06:42 +0100

    added gitignore and the asp.net api

---
+++ 3000BackendDatabaseAPI/3000BackendDatabase/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _3000BackendDatabase.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+      
+
+
+        
+
+        public IConfiguration Configuration { get; }
+
+        [HttpGet]
+        public string Get()
+        {
+            string connectionString = Configuration.GetConnectionString("Default");
+
+
+
+
+
+
+
+
+            return "hello";
+        }
+    }
+}
