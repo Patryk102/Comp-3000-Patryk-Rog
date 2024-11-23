e1f2398e26b73f28169d05e9e3e896829f85285e
Patryk102 <patryk.rog@students.plymouth.ac.uk>
2024-11-23 13:54:24 +0100

    made jwt token work

---
+++ 3000BackendDatabaseAPI/3000BackendDatabase/Controllers/LoginController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Data.SqlClient;
+using Newtonsoft.Json.Linq;
+
+
+namespace _3000BackendDatabase.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+        //private readonly UserManager<IdentityUser> _userManager;
+        private readonly IConfiguration _configuration;
+        
+        public LoginController(/*UserManager<IdentityUser> userManager,*/ IConfiguration configuration)
+        {
+            //_userManager = userManager;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public IActionResult Login([FromBody] dynamic models)
+        {
+            JObject model = JObject.Parse(models.ToString());
+            
+            string email = model["email"].ToString();
+     
+            string password = model["password"].ToString();
+            string connectionString = _configuration.GetConnectionString("Default");
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string sql = "SELECT email, password FROM BOOKING.[User] WHERE email = @email";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@email", email);
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            string dbEmail = reader["email"].ToString();
+                            string dbPassword = reader["password"].ToString();
+                            if (dbPassword == password)
+                            {
+                                var token = GenerateJwtToken(dbEmail);
+                                return Ok(new { token });
+                            }
+                        }
+                    }
+                }
+                return Unauthorized();
+
+
+            }
+
+
+
+        }
+
+
+
+
+
+        private string GenerateJwtToken(string email)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var Token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:issuer"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(Token);
+
+        }
+
+        
+
+        
+    }
+}
--- 3000BackendDatabaseAPI/3000BackendDatabase/Program.cs
+++ 3000BackendDatabaseAPI/3000BackendDatabase/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,42 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+
+
+//Added stuff starts here
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Issuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+
+        };
+    });
+
+/*
+builder.Services.AddSwaggerGen(c =>
+{
+    var xmlFile = $"[Assembly.GetExecutingAssembly().GetName().Name].xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});*/
+    
+    
+
+
+
+
+
+//ends here
 
 var app = builder.Build();
 
