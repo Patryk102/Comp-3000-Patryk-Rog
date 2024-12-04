15fd294417140091911af9184fcacd6411221620
Patryk102 <patryk.rog@students.plymouth.ac.uk>
2024-12-04 20:28:59 +0100

    setup react frontend with docker

---
+++ reactFrontend/frontend/startup.sh
@@ -0,0 +1,12 @@
+cd /usr/src/app
+#rm -rf node_modules package-lock.json
+apt-get update
+#apt-get install -y nodejs
+
+npm install
+npm update
+npm install react-scripts@latest
+
+npm install react-router-dom
+
+npm start
