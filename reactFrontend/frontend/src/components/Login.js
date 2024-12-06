7abdcfa84789d764afd7ca04d7866c21e7801c07
Patryk102 <patryk.rog@students.plymouth.ac.uk>
2024-12-06 09:18:13 +0100

    added connection to api, cors policy to fix

--- reactFrontend/frontend/src/components/Login.js
+++ reactFrontend/frontend/src/components/Login.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Login(){
+
+    const handleLogin = () => {
+        navigate = useNavigate();
+        processLogin(navigate);
+    }
+
 
     return (
         <div>
@@ -33,11 +40,46 @@ function Login(){
 
 }
 
-function processLogin(){
-    console.log("Log in is processing");
+function processLogin(navigate){
+    console.log("processing login");
+    var url = "https://localhost:7170/api/Login/restaurant/Login";
 
+    let email = document.getElementById("emailInput").value;
+    let password = document.getElementById("passwordInput").value;
 
-}
+    let postJson = {"email":"john.smith@example.com", "password":"password123"};
+    
+    apiPostConnection(url, postJson, navigate);
+  
+  };
+
+  async function apiPostConnection(endpointUrl, loginData, navigate) {
+    const response = await fetch(endpointUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(loginData),
+    });
+    const data = await response;
+    let textResponse = await data.text();
+    console.log('Recieved token:', textResponse);
+    if (data.status == 400){
+      console.log("invalid email or password");
+      //finish this and make look nice
+    }
+    else if (data.status == 200){
+      console.log(textResponse);
+      //process for correct login TO FINISH
+      //Should then go on next page
+      //navigate("");
+    }
+    else{
+      console.log("Connection error");
+      //finish and make look nice
+    }
+
+  }
 
 
 
