008f8c31bdc217cbd9c88e894c47a56f376b7a3b
Patryk102 <patryk.rog@students.plymouth.ac.uk>
2025-01-21 16:13:00 +0100

    fixed sidebar not opening and changed style

--- reactFrontend/frontend/src/components/TopNavBar.js
+++ reactFrontend/frontend/src/components/TopNavBar.js
@@ -1,13 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useLocation } from 'react-router-dom';
+import Sidebar from "./sidebar";
 import '../componentStyles/TopNavBar.css';
 
 
 
 function TopNavBar(){
     let currentPage = useLocation();
+    const [isOpen, setIsOpen] = useState(false);
 
-    const navigate = useNavigate()
+    const homepageItems = [
+        {label: "home", link: "/"},
+        {label: "about", link: ""}
+    ];
+
+    var correctItems = [];
+
+
+
+
+    const toggleSideBar = () => {
+        setIsOpen(!isOpen);
+        console.log(isOpen);
+    };
+
+    const navigate = useNavigate();
 
     const processPressLogin = () => {
         processLoginRegister("login", navigate);
@@ -30,38 +47,32 @@ function TopNavBar(){
     const initialisePage = () => {
         console.log(currentPage.pathname);
         if (currentPage.pathname == "/"){
+            correctItems = homepageItems;
             var elements = document.getElementsByClassName("loggedOutSpecific");
             for (let i = 0; i < elements.length; i++){
                 elements[i].hidden = true;
             }
         }
-    }
 
-    const checkPage = () => {
 
     }
-
-
-
 
 
 
     return (
         <nav className="navbar">
-            <div className="menu-button">
+            <div onClick={toggleSideBar} className="menu-button">
                     <span></span>
                     <span></span>
                     <span></span>
             </div>
-            <nav className="menu">
-
-            </nav>
+            
 
 
 
 
             <div className="navbar-left">
-                
+                <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
 
                 <a>Restaurant Booking System</a>
             </div>
---
+++ reactFrontend/frontend/src/components/sidebar.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import '../componentStyles/Sidebar.css';
+
+function Sidebar({ isOpen, setIsOpen }) {
+    //const [isOpen, setIsOpen] = useState(false);
+    const [menuItems, setMenuItems] = useState([]);
+    //const [isOpen, setIsOpen] = useState(isOpened)
+    let currentPage = useLocation();
+
+    const homepageItems = [
+        {label: "home", link: "/"},
+        {label: "about", link: ""}
+    ];
+
+
+    var correctItems = [];
+
+
+    const initialisePage = () => {
+        console.log(currentPage.pathname);
+        if (currentPage.pathname == "/"){
+            setMenuItems(homepageItems);
+            
+        }
+        else{
+            setMenuItems([]);
+        }
+
+
+    }
+
+
+
+    useEffect(() => {
+        initialisePage();
+        return () => {
+
+        };
+    }, [currentPage.pathname]);
+
+
+
+
+    const toggleSidebar = () => {
+        console.log(isOpen);
+        setIsOpen(!isOpen);
+    };
+
+    const closeSideBar = () => {
+        setIsOpen(false);
+    }
+
+    return (
+        
+        <div className={`sidebar ${isOpen ? 'open' : ''}`}>
+            <div onClick={closeSideBar} className="side-button">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+            </div>
+            <nav className="menu">
+                <ul>
+                    {menuItems.map((item, index) => (
+                        <li key={index}>
+                            <a>{item.label}</a>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default Sidebar;
