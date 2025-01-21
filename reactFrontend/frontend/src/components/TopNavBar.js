f4ef77d33b6225d7b21d90aeed2908b2eb22ce71
Patryk102 <patryk.rog@students.plymouth.ac.uk>
2025-01-21 13:50:56 +0100

    changed style of top nav bar

--- reactFrontend/frontend/src/components/TopNavBar.js
+++ reactFrontend/frontend/src/components/TopNavBar.js
@@ -1,9 +1,12 @@
-import React from "react";
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import '../componentStyles/TopNavBar.css';
 
 
 
 function TopNavBar(){
+    let currentPage = useLocation();
+
     const navigate = useNavigate()
 
     const processPressLogin = () => {
@@ -14,11 +17,83 @@ function TopNavBar(){
         processLoginRegister("register", navigate);
     }
 
+    useEffect(() => {
+        initialisePage();
+        return () => {
+
+        };
+    }, []);
+
+
+
+
+    const initialisePage = () => {
+        console.log(currentPage.pathname);
+        if (currentPage.pathname == "/"){
+            var elements = document.getElementsByClassName("loggedOutSpecific");
+            for (let i = 0; i < elements.length; i++){
+                elements[i].hidden = true;
+            }
+        }
+    }
+
+    const checkPage = () => {
+
+    }
+
+
+
+
+
 
     return (
+        <nav className="navbar">
+            <div className="menu-button">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+            </div>
+            <nav className="menu">
+
+            </nav>
+
+
+
+
+            <div className="navbar-left">
+                
+
+                <a>Restaurant Booking System</a>
+            </div>
+            <div className="navbar-right">
+                <div className="loggedOutSpecific">
+                    <button className="button-4" onClick={processPressLogin}>Login</button>
+                    <button className="button-4" onClick={processPressRegister}>Register</button>
+                    <button className="button-4" onClick={initialisePage}>Init</button>
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
+        </nav>
+
+
+
+
+    )
+
+/*return (
         <div>
             <button onClick={processPressLogin}>Login</button>
             <button onClick={processPressRegister}>Register</button>
+            <button>TO FINISH Staff Login</button>
 
 
         </div>
@@ -26,7 +101,10 @@ function TopNavBar(){
 
 
 
-    )
+    )*/
+
+
+
 }
 
 function processLoginRegister(action, navigate){
