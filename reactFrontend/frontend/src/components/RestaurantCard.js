5602f78ceb2a990a838706c6e4c5a5bc4c832d97
Patryk102 <patryk.rog@students.plymouth.ac.uk>
2025-01-25 16:30:05 +0100

    added restaurant cards, placeholders for now

---
+++ reactFrontend/frontend/src/components/RestaurantCard.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "../componentStyles/restaurantCard.css";
+
+function RestaurantCard() {
+    return (
+        
+        <>
+            
+            <section className = "card">
+                <div className="cardImg">
+                    <img className="cardImg" alt="restaurant" src=""/>
+                </div>
+                <div className="cardDetails">
+                    <h3 className="cardTitle">Restaurant</h3>
+
+                </div>
+
+
+
+
+
+
+            </section>
+            
+            
+            
+            
+        
+        
+        
+        
+        </>
+    )
+}
+
+export default RestaurantCard;
--- reactFrontend/frontend/src/pages/UserDashboard.js
+++ reactFrontend/frontend/src/pages/UserDashboard.js
@@ -1,11 +1,38 @@
 import React from "react";
 import TopNavBar from "../components/TopNavBar";
+import RestaurantCard from "../components/RestaurantCard";
+import "../pageStyles/UserDashboard.css";
 
 function UserDashboard(){
     return (
         <div>
             <TopNavBar></TopNavBar>
             <p>Dashboard</p>
+            <section className="cardContainer">
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+            </section>
+
         </div>
     )
 }
